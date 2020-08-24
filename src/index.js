efb1c499742a7bcd9642329a8652462eac8b47fe
Brenno C. Lins <dev@bcl-st.com.br>
2020-08-24 03:04:48 -0300

    feature: Adicionando rota de pilotos(drivers)

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ app.get('/teams', (request, response) => {
     return response.send("apiF1 OK2")
 });
 
-
+//Pilotos
+app.get('/drivres', (request, response) => {
+    return response.json({drives: 'Pilotos'});
+});
 
 app.listen(3333, () => {
     console.warn(`🚀 Backend started 🏁 API F1 🏁`)
