739da28e23f3b29c035027d07825a87680a00fdb
Brenno C. Lins <dev@bcl-st.com.br>
2020-08-24 00:09:37 -0300

    chore: Commit inicial

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get('/teams', (request, response) => {
+    return response.send("apiF1 OK")
+});
+
+
+
+app.listen(3333, () => {
+    console.warn(`🚀 Backend started 🏁 API F1 🏁`)
+});
