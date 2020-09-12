fbfe827e9c998a39c44299e8331c4cc00e7cc60b
Brenno C. Lins <dev@bcl-st.com.br>
2020-09-12 00:57:30 -0300

    Adding CORS, to free any frontend to access apiF1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { uuid } = require('uuidv4')
+const { uuid } = require('uuidv4');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 //meu banco de dados, agora vai iniciar como um array vazio
